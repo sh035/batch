f9a25be99217e99c72bde739306896bfbf979783
sh035 <saab35@naver.com>
2025-01-30 22:02:57 +0900

    feat: API 방식으로 BeforeEntity 데이터를 AfterEntity로 저장

---
+++ src/main/java/com/exercise/batch/repository/AfterRepository.java
@@ -0,0 +1,9 @@
+package com.exercise.batch.repository;
+
+import com.exercise.batch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+
+}
---
+++ src/main/java/com/exercise/batch/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package com.exercise.batch.repository;
+
+import com.exercise.batch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+
+}
